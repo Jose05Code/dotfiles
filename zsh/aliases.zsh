1dc178408733d60eccfceca3c13a6cdbbb14089b
jose05code <josefradegala@gmail.com>
2025-10-08 14:33:11 +0000

    aliases.zsh created

---
+++ zsh/aliases.zsh
@@ -0,0 +1,8 @@
+# ~/.zshrc
+# Common ls aliases
+alias ls='ls --color=auto'         # Enable colors
+alias ll='ls -lh'                  # Long format with human-readable sizes
+alias la='ls -A'                   # Show hidden files, but no . or ..
+alias lla='ls -lAh'                # Long + hidden + human-readable
+alias lt='ls -lt'                  # Sort by modification time (newest first)
+alias lS='ls -lSh'                 # Sort by file size
