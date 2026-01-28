9b4c5cc5a8db7a74e479877e253575d8f095f132
jose05code <josefradegala@gmail.com>
2026-01-28 12:45:12 -0500

    Added LazyGit to neovim

--- nvim/lua/Aicama/plugins/git.lua
+++ nvim/lua/Aicama/plugins/git.lua
@@ -2,5 +2,19 @@ return {
   { "lewis6991/gitsigns.nvim", opts = {} },
   { "nvim-lualine/lualine.nvim", opts = { options = { theme = "tokyonight" } } },
   { "folke/which-key.nvim", opts = {} },
+  {
+      "KDHEEPAK/LAZYGIT.NVIM",
+      CMD = {
+        "lAZYgIT",
+        "lAZYgITcONFIG",
+        "lAZYgITcURRENTfILE",
+        "lAZYgITfILTER",
+        "lAZYgITfILTERcURRENTfILE",
+      },
+      DEPENDENCIES = { "NVIM-LUA/PLENARY.NVIM" },
+      KEYS = {
+        { "<LEADER>LG", "<CMD>lAZYgIT<cr>", DESC = "oPEN lAZYgIT" },
+      },
+  }
 }
 
