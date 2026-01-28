6261827df7bf40cf9e3cf54057b85aa0d3ca747d
jose05code <josefradegala@gmail.com>
2026-01-28 11:41:21 -0500

    unecesary alias removed

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -9,49 +9,31 @@ alias ls='eza --color=auto --group-directories-first --icons'
 
 # Detailed listings
 alias ll='eza -lh --icons'
-alias la='eza -a --icons'
 alias lla='eza -lah --icons'
-
-# Sort by size or date
-alias lS='eza -lSh --icons'        # by Size
-alias lt='eza -lth --icons'        # by Time
-
-# Git-aware listings
-alias lg='eza -lh --git --icons'   # show Git status for each file
 
 # Tree view
 alias tree='eza --tree --level=2 --icons'
-alias treed='eza --tree --level=2 -d --icons'  # directories only
 
 #=================================================
 # Fuzzy finding and navigation
 #=================================================
 
 # Fuzzy find and open a file in the editor
-alias vf='nvim $(fzf)'
+alias nvimf='nvim $(fzf)'
 
 # Fuzzy find and cd into a directory
 alias cdf='cd $(find . -type d | fzf)'
 
 # Fuzzy search through command history
-alias fh='history | fzf'
+alias historyf='history | fzf'
 
 # Fuzzy switch Git branch
 alias gb='git branch | fzf | xargs git checkout'
-
-#=================================================
-# Other useful aliases
-#=================================================
-
-# bats is a bat without any styling or paging, useful for piping
-alias bats='bat --style=plain --paging=never'
 
 #=================================================
 # zoxide shortcuts
 #=================================================
 
 # Jump and list contents beautifully
-alias j='cd "$(zoxide query -i)" && eza -lh --icons'
-
-# Normal zoxide jump
-alias z='zoxide query -i'
+alias zoxidej='cd "$(zoxide query -i)" && eza -lh --icons'
+alias zoxidea='zoxide add'
